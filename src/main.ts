d05bfec5393b65fd1bbfd2645497d2fd69ccb1f9
Florian-DAUVERGNE <florian.dauv@gmail.com>
2024-01-31 16:27:25 +0100

    feat: add https support

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('./secrets/cert.key'),
+      cert: fs.readFileSync('./secrets/cert.crt'),
+    },
+  });
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
   await app.listen(3000);
+
+  console.log(`Aplication is running on: https://localhost:3000`);
 }
 bootstrap();

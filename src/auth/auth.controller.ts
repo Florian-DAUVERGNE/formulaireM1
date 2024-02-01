1cdbeb5ef50377fe42e49df0918d9aea47a0dbfd
Florian-DAUVERGNE <florian.dauv@gmail.com>
2024-02-01 19:48:26 +0100

    feat: added register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { UserService } from '../user/user.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly userService: UserService) {}
   @Post('login')
   @UseGuards(AuthGuard('local'))
   async login(@Request() req) {
     return req.user;
+  }
+
+  @Post('register')
+  async register(@Request() req) {
+    const user = await this.userService.create(req.body);
+    return user;
   }
 }
